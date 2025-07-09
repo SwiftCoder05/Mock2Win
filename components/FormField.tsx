10e4a30f461233ccd72848254f07f4f3a5ffad57
Sinchnarao <sinchanachummi05@gmail.com>
2025-07-09 09:20:06 +0530

    Initial Commit

---
+++ components/FormField.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import React from "react";
+import { Controller, Control, FieldValues, Path } from "react-hook-form";
+import {
+    FormControl,
+    FormDescription,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+// Generic reusable component
+interface FormFieldProps<T extends FieldValues> {
+    control: Control<T>;
+    name: Path<T>;
+    label: string;
+    placeholder?: string;
+    type?: "text" | "email" | "password" | "file";
+}
+
+const FormField = <T extends FieldValues>({
+                                              control,
+                                              name,
+                                              label,
+                                              placeholder = "",
+                                              type = "text",
+                                          }: FormFieldProps<T>) => (
+    <Controller
+        name={name}
+        control={control}
+        render={({ field }) => (
+            <FormItem>
+                <FormLabel className="label">{label}</FormLabel>
+                <FormControl>
+                    <Input type={type} placeholder={placeholder}{...field} />
+                </FormControl>
+                <FormDescription />
+                <FormMessage />
+            </FormItem>
+        )}
+    />
+);
+
+export default FormField;
